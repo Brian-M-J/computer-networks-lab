28a2665f7f5f9b86d7ae3f3fec5dde079ebe4628
Brian-M-J <1DT21AI037@dsatm.edu.in>
2024-02-21 10:48:33 +0530

    Update server.py - Correct 'Connected to client at' message

--- file_server/server/server.py
+++ file_server/server/server.py
@@ -9,7 +9,7 @@ def main():
 		print(f'File server is running on https://{host}:{port}')
 		s.listen()
 		conn, addr = s.accept()
-		print(f'Connected to client at https://{addr[0]}{addr[1]}')
+		print(f'Connected to client at https://{addr[0]}:{addr[1]}')
 		try:
 			filename = Path(conn.recv(1024).decode().strip()).name
 			with open(filename, 'rb') as f:
